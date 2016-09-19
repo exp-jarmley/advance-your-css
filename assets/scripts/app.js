f837c2c56d7f99796ff4f46e51009e9dbf9e42d6
Jack Armley <jarmley@expedia.com>
2016-09-19 12:26:47 +0100

    Edited readbutton js and markup

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,20 +6,21 @@
 (function(){
 
   var parent = document.querySelector('.postcard'),
-      trigger = document.querySelector('.postcard-readbutton'),
+      trigger = document.getElementsByClassName('postcard-readbutton'),
       isActive,
-      activeClass = 'is--flipped';
+      activeClass = 'is--flipped',
+      i = 0,
+      doFlip = function() {
+        isActive = parent.className.indexOf(activeClass);
+        if (isActive === -1) {
+          parent.classList.add(activeClass);
+        } else {
+          parent.classList.remove(activeClass);
+        }
+      };
 
-  trigger.addEventListener('click', function(){
-
-    isActive = parent.className.indexOf(activeClass);
-
-    if (isActive === -1) {
-      parent.classList.add(activeClass);
-    } else {
-      parent.classList.remove(activeClass);
-    }
-
-  });
+  for(i;i<trigger.length;i++) {
+    trigger[i].addEventListener('click', doFlip );
+  }
 
 })();
